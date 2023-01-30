0553c1d7ba3098976b78ca5a6c539cc0c153284d
torawork <123803717+torawork@users.noreply.github.com>
2023-01-30 21:37:21 +0000

    обновила главную кнопку

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -7,16 +7,15 @@ function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
-        border: 1px solid #152842;
+        background: #3B82F6;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
         color: white;
         &:hover {
-            background: linear-gradient(180deg, #4D9FF0 0%, #36699D 100%);
+            background: #5C96F5;
         }
         &:active {
-            background: #152842;
+            background: #276EE2;
         }
     `;
     return (
