27bf4baf43623451a08d64a034aeb7daef684717
torawork <123803717+torawork@users.noreply.github.com>
2023-01-30 22:07:12 +0000

    вторая кнопка

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -5,16 +5,26 @@ function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
     let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
+        background: #FFFFFF;
+        border: 1px solid #64748B;
+        border-radius: 24px;
+        padding: 8px 16px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color: #152842;
+        color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #FFFFFF;
+            border: none;
+            border-radius: 24px;
+            color: #64748B;
+            opacity: 80;
         }
         &:active {
-            background: #DFDFDF;
+            background: #FFFFFF;
+            border: none;
+            border-radius: 24px;
+            color: #64748B;
+            opacity: 60;
         }
     `;
     return (
