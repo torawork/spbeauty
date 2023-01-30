abf42b73b73b21ad10dba31117db728b8f09bdd3
torawork <123803717+torawork@users.noreply.github.com>
2023-01-30 22:37:04 +0000

    иконка стрелка вниз

--- src/Icons/ArrowDownIcon.js
+++ src/Icons/ArrowDownIcon.js
@@ -1,8 +1,8 @@
 function ArrowDownIcon() {
     return (
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12.6668 6L8.00016 10.6667L3.3335 6" stroke="#94A3B8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.8332 7.5L9.99984 13.3333L4.1665 7.5" stroke="#0F172A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
     );
   }
   
