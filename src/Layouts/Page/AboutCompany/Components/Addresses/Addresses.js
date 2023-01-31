0cbffe84b25ff2d51d1b990cce633e866ae1088b
torawork <123803717+torawork@users.noreply.github.com>
2023-01-31 22:26:40 +0000

    h

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -5,7 +5,7 @@ function Addresses() {
     return (
         <div className='d-inline-flex flex-column gap-2 mt-4'>
             <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-2 list-element'>
+            <div className='d-inline-flex flex-column gap-2'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
