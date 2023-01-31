502fbfea59f377d028fea0330c54eb635504eeaa
torawork <123803717+torawork@users.noreply.github.com>
2023-01-31 22:11:49 +0000

    col

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -3,7 +3,7 @@ import General from "./Components/General/General";
 import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
     return (
-        <div>
+        <div className='col'>
             <Heading/> {/* Заголовок и кнопка справа */}
             <div className='row mt-5'>
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
