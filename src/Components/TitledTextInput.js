6477632df92134b8a0700a1a050a4dea3b8bc5f6
torawork <123803717+torawork@users.noreply.github.com>
2023-01-31 21:08:42 +0000

    delete class

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -21,7 +21,7 @@ function TitledTextInput({title, def}) {
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Input className='auto' defaultValue={def} ></Input>
+            <Input defaultValue={def} ></Input>
         </div>
     );
 }
