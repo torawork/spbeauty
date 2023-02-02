e6b4c1856b315287179cd00a8ab6e4f6da0e79f8
torawork <123803717+torawork@users.noreply.github.com>
2023-02-02 19:02:24 +0000

    cap -

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -21,7 +21,7 @@ function TitledTextInput({title, def}) {
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Input className='px-3 py-2 caption' defaultValue={def} ></Input>
+            <Input className='px-3 py-2' defaultValue={def} ></Input>
         </div>
     );
 }
