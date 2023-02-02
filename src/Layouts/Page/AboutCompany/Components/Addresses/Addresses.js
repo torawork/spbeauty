1d74101a3b9bffae64223fad37e184ed5232f45d
torawork <123803717+torawork@users.noreply.github.com>
2023-02-02 23:10:49 +0000

    final

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,13 +3,16 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4 col new-border'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-2'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
+        <div className='d-inline-flex flex-column justify-content-between gap-2 mt-4 col new-border'>
+            <div>
+                <div className='title pb-4'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column gap-2'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
             </div>
+                    <SecondaryButton>Добавить филиал</SecondaryButton>
+                
         </div>
     );
 }
--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -12,7 +12,7 @@ const Option = styled.div`
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
+            <Option className='flex-grow-1 py-3 gap-3'>{props.children}</Option>
             <IconOnlyButton icon={<EditIcon/>}/>
             <IconOnlyButton icon={<RemoveIcon/>}/>
         </div>
