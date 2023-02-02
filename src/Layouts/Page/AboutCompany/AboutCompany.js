9c3fe40026431877d49eac89855e9ef8bc1f24fd
torawork <123803717+torawork@users.noreply.github.com>
2023-02-02 19:07:26 +0000

    row 16px

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -5,7 +5,7 @@ function AboutCompany() {
     return (
         <div>
             <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='row mt-5'>
+            <div className='row mt-3'>
                 <General/>  {/* Основная информация вроде названия, типа и телефона */}
                 <Addresses/> {/* Список доступных адресов заведений */}
             </div>
