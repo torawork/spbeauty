04dd9eedc4e010f89f7cae01c2ae747156f77096
torawork <123803717+torawork@users.noreply.github.com>
2023-02-02 18:42:37 +0000

    caption

--- src/Layouts/Page/AboutCompany/Components/General/Components/Preferences.js
+++ src/Layouts/Page/AboutCompany/Components/General/Components/Preferences.js
@@ -4,12 +4,12 @@ import PrimaryButton from "../../../../../../Components/PrimaryButton";
 function Preferences() {
     return (
         <div className='d-inline-flex flex-column gap-4 mt-5'>
-            <TitledTextInput title='Название компании' def='SPBEAUTY'/>
-            <TitledSelector title='Тип организации' options={[
+            <TitledTextInput className='caption' title='Название компании' def='SPBEAUTY'/>
+            <TitledSelector className='caption' title='Тип организации' options={[
                 'Салон красоты',
                 'Парикмахерская'
             ]}/>
-            <TitledTextInput title='Основной телефон для связи' def='+7 (912) 345-67-89'/>
+            <TitledTextInput className='caption' title='Основной телефон для связи' def='+7 (912) 345-67-89'/>
             <PrimaryButton>Сохранить</PrimaryButton>
         </div>
     );
